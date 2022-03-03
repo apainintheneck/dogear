ebbfcfe7723de82d6fe99dd3342f31d35cc8fef9
apainintheneck <apainintheneck@gmail.com>
2022-03-03 14:14:52 -0800

    Initial Commit

---
+++ dogear/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dogear
+//
+//  Created by Kevin on 3/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
